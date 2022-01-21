718a416103159465e6e6f11bdff8ff2a144e2af2
Larry Eliemenye <denachural@gmail.com>
2022-01-21 21:09:10 +0000

    debug

--- src/index.js
+++ src/index.js
@@ -2,25 +2,22 @@ const path = require('path');
 const fs = require('fs');
 
 const core = require('@actions/core');
-const github = require('@actions/github');
 const lcov = require('lcov-total');
 
 const { GITHUB_WORKSPACE } = process.env
-
-const failIfBelow = core.getInput('fail-if-below');
-try {
+(function () {
+    const failIfBelow = core.getInput('fail-if-below');
     const lcovPath = path.resolve(GITHUB_WORKSPACE, `${core.getInput('lcov-file-path')}`);
-    const lcovContent = fs.readFileSync(lcovPath);
-    console.log(lcovContent.toString('utf8'));
 
-    const total = lcov(lcovPath);
-    if(total < failIfBelow) {
-        return core.setFailed(`Code coverage constraint was not met: ${total}/${failAt}`);
+    try {
+        const lcovContent = fs.readFileSync(lcovPath);
+        console.log(lcovContent.toString('utf8'));
+
+        const total = lcov(lcovPath);
+        if(total < failIfBelow) return core.setFailed(`Code coverage constraint was not met: ${total}/${failAt}`);
+
+        core.setOutput('coverage-percent', total);
+    } catch(e) {
+        core.setFailed(`Actions Error: ${e.message}. Please make sure an lcov file was generated for your source code`)
     }
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`Event Payload: ${payload}`);
-    core.setOutput('coverage-percent', total);
-} catch(e) {
-    core.setFailed(`Actions Error: ${e.message}. Please make sure an lcov file was generated for your source code`)
-}
-
+})()
